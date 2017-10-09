faf68f470f32af10fa043723ba5d0f342666bb03
mkoertgen <marcel.koertgen@gmail.com>
2017-10-09 13:17:03 +0200

    Simplify import/export (only .override.yml needed)

--- data/export.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ash
-set -e
-
-backup=${1:-/data/registry.bz2}
-echo "Exporting docker registry to ${backup}..."
-tar -cjf ${backup} -C /var/lib/registry ./
-echo "Done."
--- data/import.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-set -e
-
-backup=${1:-/data/registry.bz2}
-echo "Importing docker images from ${backup}..."
-rm -rf /var/lib/registry/*
-tar -C /var/lib/registry -xjf ${backup}
-echo "Done."
